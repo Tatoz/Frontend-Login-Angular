1322816859d5f4f2937ac2f3f9e2f4f08829e8d0
Stempreiss <jcstempreiss@outlook.com>
2019-11-06 13:42:26 -0600

    Servicio de token

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { JarwisService } from 'src/app/Services/jarwis.service';
+import { TokenService } from 'src/app/Services/token.service';
 
 @Component({
   selector: 'app-login',
@@ -15,13 +16,19 @@ public form = {
 };
 
 public error = null;
-  constructor(private Jarwis:JarwisService) { }
+  constructor(private Jarwis:JarwisService, private Token:TokenService) {
+
+   }
 
   onSubmit(){
     this.Jarwis.signin(this.form).subscribe(
-      data => console.log(data),
+      data => this.handleResponse(data),
       error => this.handleError(error)
     );
+  }
+
+  handleResponse(data){
+    this.Token.handle(data.access_token);
   }
 
   handleError(error){
