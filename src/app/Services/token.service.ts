658a2d03ed1231253878dc14ec3485ca16210f15
Stempreiss <jcstempreiss@outlook.com>
2019-11-07 11:16:36 -0600

    Generacion de token terminada

--- src/app/Services/token.service.ts
+++ src/app/Services/token.service.ts
@@ -4,12 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenService {
-
+  private iss={
+    login:'http://localhost:8000/api/login',
+    signup: 'http://localhost:8000/api/signup'
+  }
   constructor() { }
 
   handle(token){
     this.set(token);
-    console.log(this.payload(token));
+    console.log(this.isValid());
   }
 
   set(token){
@@ -29,12 +32,10 @@ export class TokenService {
     if(this.get()){
       const payload = this.payload(token);
       if(payload){
-        payload.iss == 'http://localhost:8000/api/login' ? true:false;
-      }
-      if(token){
-
+       return Object.values(this.iss).indexOf(payload.iss) >-1 ? true:false;
       }
     }
+    return false;
   }
 
   payload(token){
@@ -46,4 +47,7 @@ export class TokenService {
     return JSON.parse(atob(payload));
   }
 
+  loggedIn(){
+    return this.isValid();
+  }
 }
